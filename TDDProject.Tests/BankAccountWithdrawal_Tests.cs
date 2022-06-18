9de01f35c639e3b380084f128b6c8d0089e347af
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 13:28:09 +0200

    Withdrawal - added test

--- TDDProject.Tests/BankAccountWithdrawal_Tests.cs
+++ TDDProject.Tests/BankAccountWithdrawal_Tests.cs
@@ -1,13 +1,32 @@
 ﻿using NUnit.Framework;
+using TDDProject.Application;
 
 namespace TDDProject.Tests
 {
     public class BankAccountWithdrawal_Tests
     {
+        private Bank Bank { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            this.Bank = new Bank();
+            Bank.CreateAccount("Gracjan", "Bryt");
+            Bank.Accounts[0].Balance = 1000.00m;
+        }
 
+        [Test]
+        public void Withdrawal_Test()
+        {
+            // Arrange
+            string myAccountNumber = Bank.Accounts[0].AccountNumber;
+            decimal withdrawal = 100.00m;
+
+            // Act
+            Bank.Withdrawal(myAccountNumber, withdrawal);
+
+            // Assert
+            Assert.AreEqual(900.00, Bank.Accounts[0].Balance);
         }
     }
 }
