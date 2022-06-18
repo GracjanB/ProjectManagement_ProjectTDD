4140516de60e9c759c9eab4eeed5b7602a369060
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 14:34:28 +0200

    Delete account - added test

--- TDDProject.Tests/BankAccountRemove_Tests.cs
+++ TDDProject.Tests/BankAccountRemove_Tests.cs
@@ -1,13 +1,36 @@
 ﻿using NUnit.Framework;
+using System.Linq;
+using TDDProject.Application;
 
 namespace TDDProject.Tests
 {
     public class BankAccountRemove_Tests
     {
+        private Bank Bank { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            this.Bank = new Bank();
 
+            Bank.CreateAccount("Gracjan", "Bryt");
+            Bank.CreateAccount("Jan", "Kowalski");
+            Bank.CreateAccount("Jan", "Nowak");
+        }
+
+        [Test]
+        public void DeleteAccount()
+        {
+            // Arrange
+            string accountNumber = Bank.Accounts[0].AccountNumber;
+            string name = "Gracjan";
+            string surname = "Bryt";
+
+            // Act
+            Bank.DeleteAccount(name, surname, accountNumber);
+
+            // Assert
+            Assert.IsNull(Bank.Accounts.SingleOrDefault(x => x.AccountNumber == accountNumber));
         }
     }
 }
