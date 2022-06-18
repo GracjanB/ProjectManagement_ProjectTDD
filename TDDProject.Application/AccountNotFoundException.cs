74eb4ba69aaff1515684752dd18d42e34efa6e04
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 12:22:34 +0200

    Payment on account - account not found - refactor

--- TDDProject.Application/AccountNotFoundException.cs
+++ TDDProject.Application/AccountNotFoundException.cs
@@ -6,20 +6,12 @@ namespace TDDProject.Application
     [Serializable]
     public class AccountNotFoundException : Exception
     {
-        public AccountNotFoundException()
-        {
-        }
+        public AccountNotFoundException() { }
 
-        public AccountNotFoundException(string message) : base(message)
-        {
-        }
+        public AccountNotFoundException(string message) : base(message) { }
 
-        public AccountNotFoundException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
+        public AccountNotFoundException(string message, Exception innerException) : base(message, innerException) { }
 
-        protected AccountNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
+        protected AccountNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
