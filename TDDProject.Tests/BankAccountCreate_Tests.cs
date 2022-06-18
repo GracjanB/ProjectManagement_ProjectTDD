06dc3ff96e85dee62d6d4f870c381c64bd1e3568
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 10:32:01 +0200

    Create account - added test

--- TDDProject.Tests/BankAccountCreate_Tests.cs
+++ TDDProject.Tests/BankAccountCreate_Tests.cs
@@ -1,13 +1,23 @@
 ﻿using NUnit.Framework;
+using TDDProject.Application;
 
 namespace TDDProject.Tests
 {
     public class BankAccountCreate_Tests
     {
+        private Bank Bank { get; set; }
+
         [SetUp]
         public void Setup()
         {
+            this.Bank = new Bank();
+        }
 
+        [Test]
+        public void CreateAccount_Test()
+        {
+            Assert.AreNotEqual(null, Bank.Accounts);
+            Assert.AreEqual(1, Bank.Accounts.Count);
         }
     }
 }
