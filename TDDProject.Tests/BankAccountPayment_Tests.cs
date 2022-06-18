c10b0527c54fcb420a58f3f5231c130ae9f7ca32
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 12:08:11 +0200

    Payment on account - added test

--- TDDProject.Tests/BankAccountPayment_Tests.cs
+++ TDDProject.Tests/BankAccountPayment_Tests.cs
@@ -1,13 +1,33 @@
 ﻿using NUnit.Framework;
+using TDDProject.Application;
 
 namespace TDDProject.Tests
 {
     public class BankAccountPayment_Tests
     {
+        private Bank Bank { get; set; }
+
         [SetUp]
         public void Setup()
         {
-
+            this.Bank = new Bank();
+            Bank.CreateAccount("Gracjan", "Bryt");
         }
+
+        [Test]
+        public void PaymentOnAccount_Test()
+        {
+            // Arrange
+            string myAccountNumber = Bank.Accounts[0].AccountNumber;
+            decimal deposit = 100.00m;
+
+            // Act
+            Bank.Deposit(myAccountNumber, deposit);
+
+            // Assert
+            Assert.AreEqual(100.00, Bank.Accounts[0].Balance);
+        }
+
+
     }
 }
