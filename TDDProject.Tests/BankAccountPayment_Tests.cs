b6a96a20e8582058a98d01b338d351e252987f65
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-18 12:17:16 +0200

    Payment on account - account not found - added test

--- TDDProject.Tests/BankAccountPayment_Tests.cs
+++ TDDProject.Tests/BankAccountPayment_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using TDDProject.Application;
 
 namespace TDDProject.Tests
@@ -26,6 +27,20 @@ namespace TDDProject.Tests
 
             // Assert
             Assert.AreEqual(100.00, Bank.Accounts[0].Balance);
+        }
+
+        [Test]
+        public void PaymentOnAccount_AccountNotFound_Test()
+        {
+            // Arrange
+            string myAccountNumber = "1345236354764314123";
+            decimal deposit = 100.00m;
+
+            // Assert
+            Assert.Throws(typeof(AccountNotFoundException), () =>
+            {
+                Bank.Deposit(myAccountNumber, deposit);
+            });
         }
 
 
